e8d8846b395ae51d315c64af1d9a732766bcb6f6
user1303836 <user1303836@gmail.com>
2026-02-14 21:21:33 -0500

    Address PR review feedback
    - Add SharedCode INTERFACE library for future test target
    - Scope push trigger to main branch only
    - Bump sccache-action to v0.0.9
    - Bump pluginval strictness to level 10
    - Fix deprecated setFont call for JUCE 8

--- source/PluginEditor.cpp
+++ source/PluginEditor.cpp
@@ -10,7 +10,7 @@ void HdnRingmodAudioProcessorEditor::paint(juce::Graphics& g)
 {
     g.fillAll(juce::Colours::black);
     g.setColour(juce::Colours::white);
-    g.setFont(24.0f);
+    g.setFont(juce::FontOptions(24.0f));
     g.drawText("HDN Ring Modulator", getLocalBounds(), juce::Justification::centred);
 }
 
